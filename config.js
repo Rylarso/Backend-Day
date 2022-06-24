2a5dbc99c389458eca0b95954f27d9a07c02505b
[ryan larson] <[rylarso@gmail.com]>
2022-06-24 09:47:18 -0600

    express listen and config set up

--- config.js
+++ config.js
@@ -1,2 +1,16 @@
+//flag configuration
+const flags = require("flags");
+flags.defineNumber("port", 8080, "Ports for the http server to listen on");
+flags.parse();
+
 //enviroment configuration
-const dotevn = require("dotenv");
+const dotenv = require("dotenv");
+
+//configure port number
+const port = flags.get("port");
+
+module.exports = {
+    flags,
+    dotenv,
+    port
+}
--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+//get configuerations 
+const config = require("./config");
 
-
+app.listen(config.port, () => {
+    console.log(`Server is running on port ${config.port}`);
+});
